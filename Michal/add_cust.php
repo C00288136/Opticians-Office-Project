782ffcbb9ef607ad7fa6b92cffdcb7502885c36c
C00288136 <C00288136@setu.ie>
2024-02-22 18:53:06 +0000

    add customer created

---
+++ Michal/add_cust.php
@@ -0,0 +1,29 @@
+<?php
+    include 'db.inc.php';
+    date_default_timezone_set("UTC");
+
+
+    echo "The details sent down are : <BR>";
+    echo "First name is : " . $_POST['firstname'] . "<BR>";
+    echo "Address is : " . $_POST["Address"] . "<BR>";
+
+
+
+
+    $sql = "INSERT into student (Name,Address,Eircode,DOB,Phone) VALUES   ('$_POST[name]','$_POST[Address]','$_POST[eircode]','$_POST[dob]','$_POST[PhoneNum]')";
+//uses info form insert.html and runs a sql query to add the data into the table
+    if (!mysqli_query($con,$sql))
+    {
+        die("An error in the SQL Query: " .mysqli_error($con));
+
+    }
+    //successfully added message 
+    echo "<BR> A record has been added for " . $_POST['firstname'] . " .";
+
+    mysqli_close($con)
+    ?>
+
+    <form action="Add_Customer.html" method="post">
+        <br>
+        <input type="submit" value="Return to Insert Page" name="" id="">
+    </form>
