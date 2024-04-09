eafc82e6846543cce45aeb48fd3c90755bc48627
karolissg <karolis.griga@gmail.com>
2024-04-09 17:31:35 +0100

    Yurr

--- Karolis/AmendStock/AmendView.php
+++ Karolis/AmendStock/AmendView.php
@@ -2,9 +2,7 @@
     include "../../db.inc.php";
 
     date_default_timezone_set('UTC');
-    $dbDate = date("Y-m-d", strtotime($_POST['amendDOB']));
-
-    $sql = "UPDATE inventory SET firstname = '{$_POST['amendfirstname']}',lastname = '{$_POST["amendlastname"]}', DOB = '$dbDate',email = '{$_POST["amendemail"]}',phone = '{$_POST["amendphone"]}' WHERE personsID = '$_POST[amendid]'";
+    $sql = "UPDATE inventory SET Description = '{$_POST['amenddescription']}',CostPrice = '{$_POST["amendcost"]}',RetailPrice = '{$_POST["amendretail"]}',ReorderQty = '{$_POST["amendretail"]}', SupplierID = '{$_POST["amendname"]}'  WHERE StockNumber = '$_POST[amendid]'";
     
     if(!mysqli_query($con, $sql))
     {
@@ -15,8 +13,7 @@
         if(mysqli_affected_rows($con) != 0)
         {
             echo mysqli_affected_rows($con)." record(s) updated <br>";
-            echo "Person ID ".$_POST['amendid'].", ".$_POST['amendfirstname'].", ".$_POST['amendlastname']
-            ."has been updated";
+            echo "Stock Number ".$_POST['amendid'].", ".$_POST['amenddescription']."has been updated";
         }
         else
         {
@@ -25,6 +22,6 @@
     }
     mysqli_close($con);
     ?>
-<form action="AmendView.html.php" method="post" />
+<form action="AmendView.html.php" method="post">
 <input type="submit" value="Return to Previous Screen">
 </form>
